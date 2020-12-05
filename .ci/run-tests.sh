48a65e5153dce47d4bded56079ddb43ba51f8df7
Martin Evans <martindevans@gmail.com>
2020-12-05 15:16:57 +0000

    Split job up into several sub steps

---
+++ .ci/run-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+YODK=../yodk
+
+function check_folder {
+
+    cd $1
+
+    ls *test.yaml &>/dev/null || (echo "No test-files found"; exit 1)
+    $YODK test *test.yaml || (echo "Tests failed"; exit 1)
+    
+    cd ..
+}
+
+for dir in */; do
+    if `cat .testignore | grep -q "$dir"`; then
+        echo "-----Skipping ignored folder: $dir -----"
+        continue
+    fi
+    echo "-----Checking folder '$dir'-----"
+    check_folder $dir
+done
---
+++ .ci/verify-yolol.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+YODK=../yodk
+
+function check_folder {
+
+    cd $1
+
+    ls *.yolol &>/dev/null || (echo "No .yolol files found"; exit 1)
+    $YODK verify *.yolol || (echo "Yolol-files are invalid"; exit 1)
+
+    cd ..
+}
+
+for dir in */; do
+    if `cat .testignore | grep -q "$dir"`; then
+        echo "-----Skipping ignored folder: $dir -----"
+        continue
+    fi
+    echo "-----Checking folder '$dir'-----"
+    check_folder $dir
+done
